87606b60b44d552acedf8c1031be90b95292efbc
InzooC <bluefreaks@gmail.com>
2022-08-21 13:38:10 +0800

    feat: add home, login page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const express = require('express')
 const { engine } = require('express-handlebars')
 
@@ -8,11 +8,23 @@ const port = 3000
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 // app.set('views', './views')
-app.use(bodyParser.urlencoded({ extend: true }))
+// app.use(bodyParser.urlencoded({ extend: true }))
+app.use(express.urlencoded({ extended: true }))
+
+// ?router區 需重構
+app.get('/adminLogin', (req, res) => {
+  res.render('admin-login')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
 
 app.get('/', (req, res) => {
   res.render('home')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Game app is listening on port ${port}`)
---
+++ leetcode.js
@@ -0,0 +1,7 @@
+var maxProfit = function (prices) {
+
+};
+
+// Input: prices = [7, 1, 5, 3, 6, 4]
+// Output: 5  (６－１＝５)
+// 最大順差
