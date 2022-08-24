2d6cb8e7715a86d506d8b34729d625207633047c
InzooC <bluefreaks@gmail.com>
2022-08-24 20:57:56 +0800

    feat: user-seed add password hash

--- seeders/20220822024945-user-seed-file.js
+++ seeders/20220822024945-user-seed-file.js
@@ -1,40 +1,39 @@
 'use strict';
 const bcrypt = require('bcrypt')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const accounts = []
+    const users = []
     for (let i = 0; i < 3; i++) {
       let account = (Math.floor(Math.random() * 899) + 100)
       if (!accounts.includes(account)) {
         accounts.push(account)
+        users.push({
+          name: `user-${i}`,
+          account: account,
+          password: await bcrypt.hash('123', 10),
+          role: 'user',
+          created_at: new Date(),
+          updated_at: new Date()
+        })
       } else {
         i--
       }
     }
-    await queryInterface.bulkInsert('Users',
-      Array.from({ length: 3 }).map((_, i) =>
-      ({
-        name: `user-${i}`,
-        account: accounts[i],
-        password: '123',
-        role: 'user',
-        created_at: new Date(),
-        updated_at: new Date()
-      }))
-      , {})
+    await queryInterface.bulkInsert('Users', users, {})
     await queryInterface.bulkInsert('Users',
       [{
         name: 'admin',
         account: 'admin123',
-        // password: await bcrypt.hash('123', 10),
-        password: '123',
+        password: await bcrypt.hash('123', 10),
         role: 'admin',
         created_at: new Date(),
         updated_at: new Date()
       }]
-      , {});
+      , {})
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Users', null, {});
   }
-};
+}
